357c0c59c73b05c692b6448f9ee59f2c295bbba6
Isaias Nogueira Santos <insantosdev@gmail.com>
2024-03-26 10:05:59 -0300

    Exercico de class 2

---
+++ exercicio2.py
@@ -0,0 +1,20 @@
+# classe de produtos - dando descontos 
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def __str__(self):
+        return f'Produto {self.name} - R$ {self.price} reais'
+    
+    def discount(self, perc_discout):
+        valorDiscount = (self.price/100) * perc_discout
+        finalPrice = self.price - valorDiscount
+        return int(finalPrice)
+    
+xbox = Product('Xbox Series X', 4500)
+iphone = Product('Iphone 14 Pro', 7500)
+print(xbox)
+print(iphone)
+print(xbox.discount(10))
+print(iphone.discount(15))
